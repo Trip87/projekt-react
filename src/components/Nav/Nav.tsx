d0ef7d4d9969d5b0927522b9d37e76a0694bcb37
Paweł <pawelchorazyczewski@gmail.com>
2024-05-25 13:38:07 +0200

    Stylowanie Mui

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,19 +1,72 @@
-import React from "react";
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 import { Link } from "react-router-dom";
+import "./Nav.scss";
 
-const Nav: React.FC = () => {
+export default function BasicMenu() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <nav data-testid="nav-test-id">
-      <ul>
-        <li>
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        Dashboard
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+          {" "}
           <Link to="/user-list">UserList</Link>
-        </li>
-        <li>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
           <Link to="/user-form">AddUser</Link>
-        </li>
-      </ul>
-    </nav>
+        </MenuItem>
+      </Menu>
+    </div>
   );
-};
+}
 
-export default Nav;
+// const Nav: React.FC = () => {
+//   return (
+
+//     // <nav
+//     // data-testid="nav-test-id">
+
+//     //   <ul>
+//     //     <li>
+//     //       <Link to="/user-list">UserList</Link>
+//     //     </li>
+//     //     <li>
+//     //       <Link to="/user-form">AddUser</Link>
+//     //     </li>
+//     //   </ul>
+
+//     // </nav>
+//   );
+// };
+
+// export default Nav;
--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -1,25 +1,61 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 import { Link } from "react-router-dom";
 import { AppContext } from "../Context/AppContext";
+import { IconButton, List, ListItem, ListItemText } from "@mui/material";
 
 const UserList = () => {
   const { userList, deleteUser } = useContext(AppContext);
 
+  const [hide, setHide] = useState(false);
+
+  const handleButtonClick = () => {
+    setHide((prev) => !prev);
+  };
+
   return (
-    <div>
-      <ul>
-        {userList.map((user) => (
-          <li key={user.id}>
-            <Link to={`/user-list/${user.id}`}>
-              {user.firstName} {user.lastName}
-            </Link>
-            <button onClick={() => deleteUser(user.id)}>USUŃ</button>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <button onClick={handleButtonClick}>Pokaż listę</button>
+      {hide && (
+        <div>
+          <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+            {userList.map((user) => (
+              <ListItem
+                key={user.id}
+                disableGutters
+                secondaryAction={
+                  <IconButton aria-label="comment">
+                    <Link to={`/user-list/${user.id}`}>
+                      {user.firstName} {user.lastName}
+                    </Link>
+                    <button onClick={() => deleteUser(user.id)}>USUŃ</button>
+                  </IconButton>
+                }
+              >
+                <ListItemText primary={`Line item ${user.id}`} />
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      )}
+    </>
   );
 };
+
+//   return (
+//     <div>
+//       <ul>
+//         {userList.map((user) => (
+//           <li key={user.id}>
+//             <Link to={`/user-list/${user.id}`}>
+//               {user.firstName} {user.lastName}
+//             </Link>
+//             <button onClick={() => deleteUser(user.id)}>USUŃ</button>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
 
 export default UserList;
